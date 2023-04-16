5d66bfb94e31e2a1c66465dab2b0d0e7197ed764
lintangtimur <lintangtimur915@gmail.com>
2023-04-16 12:50:45 +0700

    removed unused view

--- config/adminlte.php
+++ config/adminlte.php
@@ -291,21 +291,17 @@ return [
 
     'menu' => [
         // Navbar items:
-        [
-            'type'         => 'navbar-search',
-            'text'         => 'search',
-            'topnav_right' => true,
-        ],
+        // [
+        //     'type'         => 'navbar-search',
+        //     'text'         => 'search',
+        //     'topnav_right' => true,
+        // ],
         [
             'type'         => 'fullscreen-widget',
             'topnav_right' => true,
         ],
 
         // Sidebar items:
-        [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
-        ],
         [
             'text' => 'blog',
             'url'  => 'admin/blog',
